e0a951ae60a597049f2aab5fff15a81a3f7ee577
Aleksandr Kayda <aleksandr.kajda@frtk.ru>
2016-05-26 03:04:59 +0300

    Correct http headers, dir indexes

--- connection.c
+++ connection.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <dirent.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -13,7 +14,8 @@
 #include "picohttpparser.h"
 
 #define HTTP_IN 1
-#define HTTP_OUT 2
+#define HTTP_OUT_HEADER 2
+#define HTTP_OUT_CONTENT 3
 
 #define MAX_EVENTS 32
 #define BUFFER_SIZE 4096
@@ -26,6 +28,8 @@ typedef struct conn_info {
     char *buf;
     int size_buf;
     int written;
+    char *content;
+    int size_content;
     int f_map;
 } conn_info_t;
 
@@ -117,8 +121,6 @@ int accept_con(int listenfd, conn_info_t *data)
         return -1;
     }
     data->size_buf = BUFFER_SIZE;
-    data->written = 0;
-    data->f_map = 0;
 
     return 0;
 }
@@ -150,13 +152,15 @@ int recv_con(conn_info_t *info)
     return bytes;
 }
 
-int generate_responce(conn_info_t *info, const char *rel_path, size_t path_len)
+int generate_response(conn_info_t *info, const char *rel_path, size_t path_len)
 {
     /* TODO Check URL */
     struct stat st;
     int fd;
+    int status;
     void *mapping;
     char err_msg[] = "HTTP/1.0 404 Not Found\r\n";
+    char *status_msg;
     char *path = malloc(strlen(WEB_ROOT) + path_len + 2);
     sprintf(path, "%s/%.*s", WEB_ROOT, (int)path_len, rel_path);
 
@@ -168,36 +172,104 @@ int generate_responce(conn_info_t *info, const char *rel_path, size_t path_len)
         fd = open(path, O_RDONLY);
         if (fd == -1){
             perror("open path");
+            status = 404;
             goto respond_err;
         }
         mapping = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
         if (mapping == MAP_FAILED){
             perror("mmap path");
             close(fd);
+            status = 404;
             goto respond_err;
         }
         close(fd);
-        free(info->buf);
-        info->buf = mapping;
-        info->size_buf = st.st_size;
         info->written = 0;
+        info->content = mapping;
+        info->size_content = st.st_size;
         info->f_map = 1;
-        info->state = HTTP_OUT;
+        status = 200;
+    }
+    else if (S_ISDIR(st.st_mode)){
+        DIR *pdir;
+        struct dirent *pentry;
+        int size;
+        char dir_string[512];
+        pdir = opendir(path);
+        if (pdir == NULL){
+            perror("opendir");
+            status = 500;
+            goto respond_err;
+        }
+        info->content = malloc(BUFFER_SIZE);
+        size = BUFFER_SIZE;
+
+        sprintf(info->content, 
+                "<!DOCTYPE HTML>\r\n"
+                "<html>\r\n"
+                " <head>\r\n"
+                "  <title>Index of %s</title>\r\n"
+                " </head>\r\n"
+                "<body>\r\n\r\n", 
+                path);
+        info->size_content = strlen(info->content);
+
+        while (1){
+            errno = 0;
+            pentry = readdir(pdir);
+            if (errno != 0){
+                perror("readdir");
+                status = 500;
+                goto respond_err;
+            }
+            else if (pentry == NULL)
+                break;
+
+            if (pentry->d_name[0] == '.')
+                continue;
+            if (pentry->d_type == DT_DIR)
+                sprintf(dir_string, "<a href=\"%s/\">%s/</a><br>\r\n", pentry->d_name, pentry->d_name);
+            else 
+                sprintf(dir_string, "<a href=\"%s\">%s</a><br>\r\n", pentry->d_name, pentry->d_name);
+            info->size_content += strlen(dir_string);
+            if (info->size_content > size){
+                size *= 2;
+                info->content = realloc(info->content, size);
+            }
+            strcat(info->content, dir_string);
+        }
+        strcat(info->content, "</body> </html>");
+
+        closedir(pdir);
+        status = 200;
     }
     else{
         printf("Path is not a file\n");
+        status = 501;
         goto respond_err;
     }
 
-    free(path);
-    return 0;
-
 respond_err:
-    strcpy(info->buf, err_msg);
-    info->size_buf = strlen(err_msg);
+    switch (status){
+        case 200:
+            status_msg = "200 OK";
+            break;
+        case 404:
+            status_msg = "404 Not Found";
+            break;
+        case 501:
+            status_msg = "501 Not Implemented";
+            break;
+        default:
+            status_msg = "500 Internal Server Error";
+    }
+    sprintf(info->buf, 
+            "HTTP/1.0 %s\r\n"
+            "Content-Length: %u\r\n\r\n",
+        //    "Content-Type: text/html\r\n\r\n",
+            status_msg, info->size_content);
+    info->size_buf = strlen(info->buf);
     info->written = 0;
-    info->f_map = 0;
-    info->state = HTTP_OUT;
+    info->state = HTTP_OUT_HEADER;
     free(path);
     return 0;
 }
@@ -257,6 +329,7 @@ int main()
         for (i = 0; i < n; i++){
             if (events[i].data.ptr == NULL){
                 node = malloc(sizeof(*node));
+                memset(node, 0, sizeof(*node));
                 if (accept_con(listenfd, &node->data) == -1){
                     free(node);
                     goto conn_err;
@@ -270,10 +343,9 @@ int main()
                     perror("epoll ctl");
                     goto conn_err;
                 }
-
                 continue;
             }
-            if (events[i].events & EPOLLIN){
+            else if (events[i].events & EPOLLIN){
                 info = events[i].data.ptr;
                 printf("ep: recv from %d fd, info %p\n", info->sockfd, info);
                 if (recv_con(info) == -1){
@@ -289,9 +361,8 @@ int main()
                     printf("Parsing error!\n");
                 }
                 else if (parsed > 0){
-                    printf("Request completed: %.*s\n", (int)path_len, path);
-                    generate_responce(info, path, path_len);
-                    /* path and old buffer is freed */
+                    printf("Request path: %.*s\n", (int)path_len, path);
+                    generate_response(info, path, path_len);
 
                     ev.events = EPOLLOUT;
                     ev.data.ptr = info;
@@ -301,17 +372,34 @@ int main()
                     }
                 }
             }
-            if (events[i].events & EPOLLOUT){
+            else if (events[i].events & EPOLLOUT){
+                char *ptr;
+                int size;
                 info = events[i].data.ptr;
-                bytes = send(info->sockfd, info->buf + info->written, 
-                             info->size_buf - info->written, 0);
+
+                if (info->state == HTTP_OUT_HEADER){
+                    ptr = info->buf + info->written;
+                    size = info->size_buf - info->written;
+                }
+                else {
+                    ptr = info->content + info->written;
+                    size = info->size_content - info->written;
+                }
+                bytes = send(info->sockfd, ptr, size, 0);
                 if (bytes == -1){
                     perror("recv");
                     goto conn_err;
                 }
                 info->written += bytes;
+
                 printf("ep: sent %db to %d fdm info %p\n", bytes, info->sockfd, info);
-                if (info->written == info->size_buf){
+                if (info->written == size && info->state == HTTP_OUT_HEADER 
+                        && info->size_content)
+                {
+                    info->written = 0;
+                    info->state = HTTP_OUT_CONTENT;
+                }
+                else if (info->written == size){
                     if (epoll_ctl(epfd, EPOLL_CTL_DEL, info->sockfd, NULL) == -1){
                         perror("epoll ctl");
                         goto conn_err;
@@ -321,17 +409,20 @@ int main()
                     shutdown(info->sockfd, SHUT_RDWR);
                     close(info->sockfd);
 
-                    if (info->f_map)
-                        munmap(info->buf, info->size_buf);
-                    else 
-                        free(info->buf);
+                    if (info->content){
+                        if (info->f_map)
+                            munmap(info->content, info->size_content);
+                        else 
+                            free(info->content);
+                    }
+                    free(info->buf);
                     node = (void*)((intptr_t)info + (intptr_t)lhead - (intptr_t)&lhead->data);
                     list_del(&lhead, node);
                     free(node);
                 }
                 continue;
             }
-            if (events[i].events & EPOLLERR){
+            else if (events[i].events & EPOLLERR){
                 info = events[i].data.ptr;
                 printf("error event on socket %d!\n", info->sockfd);
             }
@@ -345,10 +436,13 @@ conn_err:
     for (node = lhead; node; node = node->next){
         shutdown(node->data.sockfd, SHUT_RDWR);
         close(node->data.sockfd);
-        if (node->data.f_map)
-            munmap(node->data.buf, node->data.size_buf);
-        else 
-            free(node->data.buf);
+        if (node->data.content){
+            if (node->data.f_map)
+                munmap(node->data.content, node->data.size_content);
+            else 
+                free(node->data.content);
+        }
+        free(node->data.buf);
         free(node);
     }
     close(listenfd);
